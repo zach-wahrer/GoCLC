4dba84d497b53d3aa546167b6316fcc2b8452082
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-09 13:45:33 -0600

    Move server messages into own file

--- server/server.go
+++ server/server.go
@@ -7,16 +7,6 @@ import (
 	"net"
 )
 
-const serverGreeting = "Welcome to the GoCLC Server!\n"
-const askUsername = "Please enter a username:\n"
-const userGreeting = "Hello,"
-const userGreetingPunc = "!\n"
-const serverGoodbye = "Goodbye!\n"
-const helpMessage = "Available Commands:\n" +
-	"/greet - show server welcome message\n" +
-	"/help - prints this help message\n"
-const unknownCommandError = "unknown command"
-
 // Listen for incoming connections
 func Listen(address, port string) {
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", address, port))
---
+++ server/server_messages.go
@@ -0,0 +1,15 @@
+package server
+
+const serverGreeting = "Welcome to the GoCLC Server!\n"
+
+const askUsername = "Please enter a username:\n"
+const userGreeting = "Hello,"
+const userGreetingPunc = "!\n"
+
+const serverGoodbye = "Goodbye!\n"
+
+const helpMessage = "Available Commands:\n" +
+	"/greet - show server welcome message\n" +
+	"/help - prints this help message\n"
+
+const unknownCommandError = "unknown command"
