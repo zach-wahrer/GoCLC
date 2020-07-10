40a7e27f5816983a247b0140716cabeaa3c7e2c1
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-10 12:15:15 -0600

    Add sleep for more consistent main testing

--- main_test.go
+++ main_test.go
@@ -5,12 +5,14 @@ import (
 	"goclctest"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestCommandLineFlags(t *testing.T) {
 	os.Args = append(os.Args, fmt.Sprintf("-address=%s", goclctest.Address))
 	os.Args = append(os.Args, fmt.Sprintf("-port=%s", goclctest.Port))
 	go main()
+	time.Sleep(5 * time.Millisecond)
 	conn := goclctest.CreateTestConnection(t)
 	goclctest.SendInputToServer(t, conn, "TestUserName")
 	goclctest.SendInputToServer(t, conn, "/exit")
