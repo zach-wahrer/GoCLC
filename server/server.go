da65d0a2f47b35a4dc375e7d393b1903278cea69
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-10 09:47:10 -0600

    Update package documentation

--- server/server.go
+++ server/server.go
@@ -1,3 +1,4 @@
+// Package server provides a stand-alone server for the GoCLC chat service.
 package server
 
 import (
@@ -7,7 +8,8 @@ import (
 	"net"
 )
 
-// Listen for incoming connections
+// Listen checks for connections on the given address and port and then
+// spawns a goroutine to handle each client separately via helper functions.
 func Listen(address, port string) {
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", address, port))
 	if err != nil {
