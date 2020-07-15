c3b2175a4c5eab8e0f7760f7159b8416aa0ae436
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-15 10:00:01 -0600

    Add test for broadcast receiving

--- server/broadcaster_test.go
+++ server/broadcaster_test.go
@@ -1,1 +1,27 @@
 package server
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestBroadcastRecieve(t *testing.T) {
+	var got bytes.Buffer
+	log.SetOutput(&got)
+
+	send := make(chan string)
+	recieve := make(chan string)
+	go startBroadcaster(send, recieve)
+
+	testMessage := "Test message"
+	recieve <- testMessage
+
+	log.SetOutput(os.Stderr)
+
+	if !strings.Contains(got.String(), testMessage) {
+		t.Errorf("broadcast error - want: %s, got: %s", testMessage, got.String())
+	}
+}
