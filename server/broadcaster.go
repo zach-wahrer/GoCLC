46b16dd5ce5eba4c4ede84f6d357c68f2f6d04d6
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-15 12:44:02 -0600

    Change broadcast from function to method

--- server/broadcaster.go
+++ server/broadcaster.go
@@ -5,28 +5,28 @@ type Broadcaster struct {
 	recieve chan string
 }
 
-func startBroadcaster() *Broadcaster {
-	b := newBroadcaster()
-	go broadcast(b)
-	return b
-
-}
-
-func broadcast(b *Broadcaster) {
-	for {
-		message := <-b.recieve
-		for _, client := range b.clients {
-			client.Write(message)
-		}
-	}
-}
-
 // NewBroadcaster constructs a new Broadcaster.
 func newBroadcaster() *Broadcaster {
 	var b Broadcaster
 	b.clients = make(map[string]*Client)
 	b.recieve = make(chan string)
 	return &b
+}
+
+func startBroadcaster() *Broadcaster {
+	b := newBroadcaster()
+	go b.broadcast()
+	return b
+
+}
+
+func (b *Broadcaster) broadcast() {
+	for {
+		message := <-b.recieve
+		for _, client := range b.clients {
+			client.Write(message)
+		}
+	}
 }
 
 func (b *Broadcaster) addClient(client *Client) {
