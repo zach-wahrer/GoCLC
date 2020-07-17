c0c2c20ff130a0aa0e954b0f9dc69cea5135b96b
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-17 07:58:41 -0600

    Add comments for exported funcs/vars

--- goclctest/functions.go
+++ goclctest/functions.go
@@ -9,10 +9,16 @@ import (
 	"testing"
 )
 
+// Address is a testing constant for use in goclctest and goclcserver
 const Address = "localhost"
+
+// Port is a testing constant for use in goclctest and goclcserver
 const Port = "9000"
+
+// TestUsername is a testing constant for use in goclctest and goclcserver
 const TestUsername = "TestUsername"
 
+// ReadyTestConnection creates a test connection to server, then logs in.
 func ReadyTestConnection(t *testing.T) (net.Conn, bufio.Scanner) {
 	conn := CreateTestConnection(t)
 	receive := bufio.NewScanner(conn)
@@ -24,6 +30,7 @@ func ReadyTestConnection(t *testing.T) (net.Conn, bufio.Scanner) {
 	return conn, *receive
 }
 
+// CreateTestConnection returns a test connection to the specified address/port.
 func CreateTestConnection(t *testing.T) net.Conn {
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", Address, Port))
 	if err != nil {
@@ -32,16 +39,19 @@ func CreateTestConnection(t *testing.T) net.Conn {
 	return conn
 }
 
+// SendInputToServer writes a string to a connection.
 func SendInputToServer(t *testing.T, conn net.Conn, input string) {
 	if _, err := io.WriteString(conn, input); err != nil {
 		InternalServerError(t, err)
 	}
 }
 
+// UnexpectedServerReplyError creates a testing error.
 func UnexpectedServerReplyError(t *testing.T, want, got string) {
 	t.Errorf("unexpected server reply: want \"%s\", got \"%s\"", want, got)
 }
 
+// InternalServerError creates a testing error.
 func InternalServerError(t *testing.T, err error) {
 	t.Errorf("internal server error: %v", err)
 }
