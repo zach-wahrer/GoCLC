ce34e3dc15c6f9cd4084f91128581cffd97b6bec
Zach Wahrer <zachtheclimber@gmail.com>
2020-07-30 12:40:21 -0600

    Remove old, non-functioning tests

--- client/client_test.go
+++ client/client_test.go
@@ -2,9 +2,8 @@ package client
 
 import (
 	"bufio"
-	"bytes"
+
 	"goclctest"
-	"io"
 	"os"
 	"server"
 	"strings"
@@ -32,49 +31,4 @@ func TestBasicClientReceive(t *testing.T) {
 		t.Errorf("client received unexpected reply - want: %s, got: %s", server.ServerGreeting, receive.Text())
 	}
 	conn.Close()
-}
-
-// func TestAdvancedClientReceive(t *testing.T) {
-// 	out := captureStdout(func() {
-// 		c := NewClient(goclctest.Address, goclctest.Port)
-// 		c.Start()
-// 	})
-// 	if !strings.Contains(out, server.ServerGreeting) {
-// 		t.Errorf("client received unexpected reply - want: %s, got: %s", server.ServerGreeting, out)
-// 	}
-// 	if !strings.Contains(out, server.AskUsername) {
-// 		t.Errorf("client received unexpected reply - want: %s, got: %s", server.AskUsername, out)
-// 	}
-// }
-//
-// func TestClientSend(t *testing.T) {
-// 	out := captureStdout(func() {
-// 		c := NewClient(goclctest.Address, goclctest.Port)
-// 		c.input = bytes.NewBufferString(goclctest.TestUsername + "\n" + "/exit")
-// 		go c.Start()
-// 	})
-// 	if !strings.Contains(out, server.UserGreeting) {
-// 		t.Errorf("client received unexpected reply - want: %s, got: %s", server.UserGreeting+" "+goclctest.TestUsername, out)
-// 	}
-// }
-
-func captureStdout(f func()) string {
-	old := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	go f()
-	time.Sleep(5 * time.Millisecond)
-	outC := make(chan string)
-
-	go func() {
-		var buf bytes.Buffer
-		io.Copy(&buf, r)
-		outC <- buf.String()
-	}()
-
-	w.Close()
-	os.Stdout = old
-	out := <-outC
-	return out
 }
